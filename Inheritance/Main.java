de814b60db6b7c2c71d4ad1e536c04530e7bcd4a
saipriyareddy1 <saipriyareddy.1119@gmail.com>
2024-01-12 00:26:26 -0600

    OOPS concepts java programs created

---
+++ Inheritance/Main.java
@@ -0,0 +1,15 @@
+abstract class Department {
+    int size = 120;
+    abstract int getDepartmentSize();
+}
+class Deptsize extends Department{
+    public int getDepartmentSize(){
+        return size;
+    }
+}
+public class Main{
+  public static void main(String args[]) {
+      Deptsize dept = new Deptsize();
+      System.out.println(dept.getDepartmentSize());
+}
+}
