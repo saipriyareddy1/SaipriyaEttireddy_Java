cb407e5f62086dc5b70f00e75e5d644da030df21
saipriyareddy1 <saipriyareddy.1119@gmail.com>
2024-01-27 21:28:43 -0600

    programs changed

--- src/main/java/maps/DistinctChar.java
+++ src/main/java/maps/DistinctChar.java
@@ -1,18 +1,21 @@
 package maps;
-
-import sun.jvm.hotspot.debugger.win32.coff.DebugVC50SSSrcLnSeg;
-
 import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.isNull;
 
 public class DistinctChar {
-    public static void differentOccur(String str){
+    public static HashMap<Character,Integer> differentOccur(String str){
         HashMap<Character,Integer> charcount = new HashMap<>();
-        char[] ch = str.toCharArray();
+        String strlower = str.toLowerCase();
+        char[] ch = strlower.toCharArray();
         for(Character letter:ch){
             if(charcount.containsKey(letter))
                 charcount.put(letter,charcount.get(letter)+1);
-            else
+            else if(letter!=' ')
                 charcount.put(letter,1);
         }
+        return charcount;
+        }
+
     }
-}
--- src/main/java/maps/ReturnStudentObject.java
+++ src/main/java/maps/ReturnStudentObject.java
@@ -1,15 +1,14 @@
 package maps;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ReturnStudentObject {
       public static HashMap<String, Student> studentObjects() {
-          String[] firstnames = {"a", "b","c","d","e","f","g","h"};
-          String[] lastnames = {"q", "v","w","x","y","z","k","j"};
-          double[] gpa = {3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7};
           HashMap<String, Student> studentobj = new HashMap<>();
-          for(int i=0;i<firstnames.length;i++){
-              studentobj.put(firstnames[i],new Student(firstnames[i],lastnames[i],gpa[i]));
+          for (Map.Entry<String, Student> entry : studentobj.entrySet()) {
+              Student student = entry.getValue();
+              studentobj.put(student.getFirstName(),student);
           }
           return studentobj;
       }
