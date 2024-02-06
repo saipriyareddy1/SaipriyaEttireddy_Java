1e57831487f75e3b52818fb8342f685abe5d4ef2
saipriyareddy1 <saipriyareddy.1119@gmail.com>
2024-02-06 15:33:34 -0600

    lamdas updated

---
+++ src/main/java/lamdas/Addition.java
@@ -0,0 +1,6 @@
+package lamdas;
+public class Addition {
+    public int performAddition(MathOperation addition, int a, int b) {
+        return addition.operate(a, b);
+    }
+}
---
+++ src/main/java/lamdas/MathOperation.java
@@ -0,0 +1,5 @@
+package lamdas;
+@FunctionalInterface
+public interface MathOperation {
+        int operate(int a, int b);
+}
