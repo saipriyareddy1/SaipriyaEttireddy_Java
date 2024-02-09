dfa8feef21655e47e16441c1adbe1f3011c3e0e0
saipriyareddy1 <saipriyareddy.1119@gmail.com>
2024-02-09 15:16:40 -0600

    threads created

--- src/main/java/threading/Deadlocks.java
+++ src/main/java/threading/Deadlocks.java
@@ -2,43 +2,34 @@ package threading;
 public class Deadlocks {
     private final Object lock1 = new Object();
     private final Object lock2 = new Object();
-    public String method1() {
-        StringBuilder result = new StringBuilder();
 
+    public void method1() {
         synchronized (lock1) {
-            result.append(Thread.currentThread().getName()).append(" acquired lock1\n");
+            System.out.println(Thread.currentThread().getName() + " acquired lock1");
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            result.append(Thread.currentThread().getName()).append(" waiting for lock2\n");
-
+            System.out.println(Thread.currentThread().getName() + " waiting for lock2");
             synchronized (lock2) {
-                result.append(Thread.currentThread().getName()).append(" acquired lock2\n");
+                System.out.println(Thread.currentThread().getName() + " acquired lock2");
             }
         }
-
-        return result.toString();
     }
 
-    public String method2() {
-        StringBuilder result = new StringBuilder();
-
+    public void method2() {
         synchronized (lock2) {
-            result.append(Thread.currentThread().getName()).append(" acquired lock2\n");
+            System.out.println(Thread.currentThread().getName() + " acquired lock2");
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            result.append(Thread.currentThread().getName()).append(" waiting for lock1\n");
-
+            System.out.println(Thread.currentThread().getName() + " waiting for lock1");
             synchronized (lock1) {
-                result.append(Thread.currentThread().getName()).append(" acquired lock1\n");
+                System.out.println(Thread.currentThread().getName() + " acquired lock1");
             }
         }
-        return result.toString();
     }
 }
-
